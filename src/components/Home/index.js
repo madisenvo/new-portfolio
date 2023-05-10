1a700460e42a6be8eaf23808de18a959dae0ac2d
Maddy <madisen@getcoins.com>
2023-05-10 16:03:04 -0600

    added pic

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,39 +3,51 @@ import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-m.png'
+import Me from '../../assets/images/MyFace.png'
 import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['a', 'd', 'i', 's', 'e', 'n', '.']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a', 'd', 'i', 's', 'e', 'n', '.']
 
-    // useEffect(() => {
-    //     return setTimeout(() => {
-    //       setLetterClass('text-animate-hover')
-    //     }, 4000)
-    //   }, []);
+  // useEffect(() => {
+  //     return setTimeout(() => {
+  //       setLetterClass('text-animate-hover')
+  //     }, 4000)
+  //   }, []);
 
-    return(
-        <div className="container home-page">
-            <div className='text-zone'> 
-                <h1> 
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt='developer' />
-                <AnimatedLetters letterClass={letterClass} 
-                strArray={nameArray}
-                idx={15}
-                />
-                </h1>
-                <h2> Fullstack Developer</h2>
-                <Link to='/contact' className='flat-button'>contact me</Link>
-            </div>
+  return (
+    <div className='container'>
+        
+      <div className="home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </h1>
+          <h2> Fullstack Developer</h2>
+          <Link to="/contact" className="flat-button">
+            contact me
+          </Link>
         </div>
-    )
+      </div>
+
+        <div className="selfie">
+          <img src={Me} alt="my face" />
+        </div>
+
+    </div>
+  )
 }
 
 export default Home
