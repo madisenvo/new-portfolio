fec3095ee2a8317db460c242b07ae3b58932851f
Madisen <madisenvogel@gmail.com>
2023-05-11 19:07:32 -0600

    about page progress

---
+++ src/components/About/index.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNode,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass] = useState('text-animate')
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+            <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+            </h1>
+          <p>
+            I'm an ambitious and motivated fullstack developer looking for a role that provides the opportunity to work with a supportive team on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm naturally curious, a passionate problem-solver, and expert communicator always looking to expand my knowledge and skillset.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNode} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="ball-rotate" />
+    </>
+  )
+}
+
+export default About
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,49 +4,43 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-m.png'
 import Me from '../../assets/images/MyFace.png'
-import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass] = useState('text-animate')
   const nameArray = ['a', 'd', 'i', 's', 'e', 'n', '.']
 
-  // useEffect(() => {
-  //     return setTimeout(() => {
-  //       setLetterClass('text-animate-hover')
-  //     }, 4000)
-  //   }, []);
-
   return (
-    <div className='container'>
-        
-      <div className="home-page">
-        <div className="text-zone">
-          <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img src={LogoTitle} alt="developer" />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-          </h1>
-          <h2> Fullstack Developer</h2>
-          <Link to="/contact" className="flat-button">
-            contact me
-          </Link>
+    <>
+      <div className="container">
+        <div className="home-page">
+          <div className="text-zone">
+            <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+              <br />
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m</span>
+              <img src={LogoTitle} alt="developer" />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+              />
+            </h1>
+            <h2> Fullstack Developer</h2>
+            <Link to="/contact" className="flat-button">
+              contact me
+            </Link>
+          </div>
         </div>
-      </div>
 
         <div className="selfie">
           <img src={Me} alt="my face" />
         </div>
-
-    </div>
+      </div>
+      <Loader type="ball-rotate" />
+    </>
   )
 }
 
