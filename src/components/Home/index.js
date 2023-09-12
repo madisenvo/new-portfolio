cc5b96cf19d8499006c7fbbf7fca4311b357a491
Maddy <madisen@getcoins.com>
2023-09-12 14:53:12 -0600

    home letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -25,19 +25,20 @@ const Home = () => {
       <div className="container">
         <div className="home-page">
           <div className="text-zone">
-            <h1>
-              <span className={letterClass}>H</span>
-              <span className={`${letterClass} _12`}>i,</span>
-              <br />
-              <span className={`${letterClass} _13`}>I</span>
-              <span className={`${letterClass} _14`}>'m</span>
-              <img src={LogoTitle} alt="developer" />
-              <AnimatedLetters
-                letterClass={letterClass}
+          <h1> 
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}>,</span>
+                <br />
+                <span className={`${letterClass} _14`}>I</span>
+                <span className={`${letterClass} _15`}>'</span>
+                <span className={`${letterClass} _16`}>m</span>
+                <img src={LogoTitle} alt='developer' />
+                <AnimatedLetters letterClass={letterClass} 
                 strArray={nameArray}
-                idx={15}
-              />
-            </h1>
+                idx={18}
+                />
+                </h1>
             <h2> Fullstack Developer</h2>
             <Link to="/contact" className="flat-button">
               contact me
