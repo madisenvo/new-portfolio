f347384402bddfd903c786511dbb0da4af252774
Maddy <madisen@getcoins.com>
2023-09-20 09:15:01 -0600

    changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,67 +5,92 @@ import LogoM from '../../assets/images/logo-m.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
+  const [menuVisible, setMenuVisible] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible)
+  }
+
   return (
-    <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={LogoM} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="madisen" />
-      </Link>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
+    <>
+      <div className={`nav-bar ${menuVisible ? 'menu-open' : ''}`}>
+        <Link className="logo" to="/">
+          <img src={LogoM} alt="logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="madisen" />
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
 
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        
-        <NavLink
-          activeclassname="active"
-          className="portfolio-link"
-          to="/portfolio"
-        >
-          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-        </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
 
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/madisen-vogel/"
+          <NavLink
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
           >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/madisenvo"
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
           >
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
-        </li>
-      </ul>
-    </div>
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/madisen-vogel/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/madisenvo"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <button
+        className={`menu-toggle ${menuVisible ? 'active' : ''}`}
+        onClick={toggleMenu}
+      >
+        {menuVisible ? (
+          <FontAwesomeIcon icon={faTimes} color="#fff" /> // Use faTimes for the "X" icon
+        ) : (
+          <FontAwesomeIcon icon={faBars} color="#fff" /> // Use faBars for the default hamburger icon
+        )}
+      </button>
+    </>
   )
 }
 
